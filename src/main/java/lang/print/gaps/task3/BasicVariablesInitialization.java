0bc247feb4b808c730fac66a268e8771255de152
v-nebe <katya.shauneva@gmail.com>
2022-08-27 10:14:48 +0300

    tasks have been completed

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -3,9 +3,9 @@ package lang.print.gaps.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         int first = 1;
-        int second =10;
+        int second = 10;
         int third = 100;
-        System.out.println(first + "\n" + second + "\n" +third);
+        System.out.println(first + "\n" + second + "\n" + third);
 
     }
 }
