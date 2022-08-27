50d371a07e1bd1d1e4c01f8efc696326dddd8d9f
v-nebe <katya.shauneva@gmail.com>
2022-08-27 10:01:25 +0300

    tasks have been completed

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -13,8 +13,7 @@ public class ReassigningValues {
         first = 15;
         second = 6;
         third = 4;
-        System.out.println(first + "\n" + second + "\n" +third + "\n"
-                +linkToFirst + "\n" + linkToSecond + "\n" + linkToThird
-                + "\n" + first + "\n" + second + "\n" + third );
+        System.out.println(first + "\n" + second + "\n" + third + "\n" + linkToFirst + "\n" + linkToSecond
+                        + "\n" + linkToThird);
     }
 }
